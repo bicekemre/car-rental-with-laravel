5b55599d9135d0b055c861388be36fa216a0b4af
bicekemre <bicekemre23@gmail.com>
2023-10-16 16:42:25 +0300

    #8692xvrz3

--- resources/views/layouts/navbar-two.blade.php
+++ resources/views/layouts/navbar-two.blade.php
@@ -15,30 +15,30 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
-                <li class="active">
+                <li class="{{ Request::is('home*') ? 'active' : '' }}">
                     <a href="{{ route('home', app()->getLocale()) }}">{{ __('navbar.home') }}</a>
                 </li>
-                <li class="">
+                <li class="{{ Request::is('cars*') ? 'active' : '' }}">
                     <a href="{{ route('cars', app()->getLocale()) }}">{{ __('navbar.cars') }}</a>
                 </li>
-                <li class="">
-                    <a href="/blog">{{ __('navbar.blog') }}</a>
+                <li class="{{ Request::is('blog*') ? 'active' : '' }}">
+                    <a href="{{ route('blog', app()->getLocale()) }}">{{ __('navbar.blog') }}</a>
                 </li>
-                <li class="">
-                    <a href="/contact">{{ __('navbar.contact') }}</a>
+                <li class="{{ Request::is('contact*') ? 'active' : '' }}">
+                    <a href="{{ route('contact', app()->getLocale()) }}">{{ __('navbar.contact') }}</a>
                 </li>
-                <li class="">
-                    <a href="/about-us">{{ __('navbar.about us') }}</a>
+                <li class="{{ Request::is('aboutus*') ? 'active' : '' }}">
+                    <a href="{{ route('aboutus', app()->getLocale()) }}">{{ __('navbar.about us') }}</a>
                 </li>
                 @if(auth()->user())
-                    <li class=""><a href="{{ route('profile', app()->getLocale()) }}">
+                    <li class="{{ Request::is('profile*') ? 'active' : '' }}"><a href="{{ route('profile', app()->getLocale()) }}">
                             {{ __('navbar.profile') }}</a>
                     </li>
-                    <li class=""><a href="{{ route('logout', app()->getLocale()) }}">
+                    <li class="{{ Request::is('logout*') ? 'active' : '' }}"><a href="{{ route('logout', app()->getLocale()) }}">
                             {{ __('navbar.logout') }}</a>
                     </li>
                 @else
-                    <li class="login-register-link right-side-link"><a href="{{ route('registration', app()->getLocale()) }}">
+                    <li class="login-register-link right-side-link {{ Request::is('registration*') ? 'active' : '' }}"><a href="{{ route('registration', app()->getLocale()) }}">
                             <i class="icon_lock-open_alt"></i>Login</a>
                     </li>
                 @endif
