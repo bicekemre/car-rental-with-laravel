6fe02f5f297947f6455e869d69545be1d1db1ad8
bicekemre <bicekemre23@gmail.com>
2023-10-17 17:10:05 +0300

    #8692y519n

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,27 @@
+<div class="sidebar">
+    <!-- start with head -->
+    <div class="head">
+        <div class="logo">
+            <img src="{{ asset('img/logo-admin.png') }}" alt="">
+        </div>
+        <a href="#" class="btn btn-danger">SUBMIT new MOVIE</a>
+    </div>
+    <!-- end with head -->
+    <!-- start the list -->
+    <div id="list">
+        <ul class="nav flex-column">
+            <li class="nav-item"><a href="index.html" class="nav-link active" ><i class="fa fa-adjust"></i>Dashboard</a></li>
+            <!-- end user interface submenu -->
+            <!-- start with charts -->
+            <li class="nav-item"><a href="#menu3" class="nav-link collapsed" data-toggle="collapse"><i class="fa fa-fire"></i>ui<span class="sub-ico"><i class="fa fa-angle-down"></i></span></a></li>
+            <!-- start charts submenue -->
+            <!-- end charts submenue -->
+            <!-- end with charts -->
+            <li class="nav-item"><a href="inbox.html" class="nav-link">Cars</a></li>
+            <li class="nav-item"><a href="contacts.html" class="nav-link"><i class="fa fa-user"></i>Users</a></li>
+            <li class="nav-item"><a href="forms.html" class="nav-link"><i class="fa fa-edit"></i>Locations</a></li>
+            <li class="nav-item"><a href="tabels.html" class="nav-link"><i class="fa fa-table"></i>Reservations</a></li>
+        </ul>
+    </div>
+    <!-- end the list -->
+</div>
