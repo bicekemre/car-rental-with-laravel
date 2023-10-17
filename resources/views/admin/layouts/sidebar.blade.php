a3f8efd4823ba6d805c2a0090c5925ebfb2333ed
bicekemre <bicekemre23@gmail.com>
2023-10-17 17:18:46 +0300

    #8692y519n

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -10,13 +10,7 @@
     <!-- start the list -->
     <div id="list">
         <ul class="nav flex-column">
-            <li class="nav-item"><a href="index.html" class="nav-link active" ><i class="fa fa-adjust"></i>Dashboard</a></li>
-            <!-- end user interface submenu -->
-            <!-- start with charts -->
-            <li class="nav-item"><a href="#menu3" class="nav-link collapsed" data-toggle="collapse"><i class="fa fa-fire"></i>ui<span class="sub-ico"><i class="fa fa-angle-down"></i></span></a></li>
-            <!-- start charts submenue -->
-            <!-- end charts submenue -->
-            <!-- end with charts -->
+            <li class="nav-item"><a href="{{ route('dashboard') }}" class="nav-link active" ><i class="fa fa-adjust"></i>Dashboard</a></li>
             <li class="nav-item"><a href="inbox.html" class="nav-link">Cars</a></li>
             <li class="nav-item"><a href="contacts.html" class="nav-link"><i class="fa fa-user"></i>Users</a></li>
             <li class="nav-item"><a href="forms.html" class="nav-link"><i class="fa fa-edit"></i>Locations</a></li>
